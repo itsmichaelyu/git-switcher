4c1ed35136744f9c0371ae4ff67c1361bd0147b4
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2021-12-05 19:11:53 +0000

    Updated for HTTPS URLs and email location
    Added loops to search through for specific keywords for both email and URL

--- gitSwitcher.zsh
+++ gitSwitcher.zsh
@@ -10,18 +10,34 @@ if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" ]; then
 		fi
 	done
 
-	SITE=$(echo $URL | cut -c12-17)
-	EMAIL=$(sed -n 7p ~/.gitconfig)
+	for i in {1..$[ $(wc -l < ~/.gitconfig)]}; do
+		EMAIL=$(sed -n $[$i]p ~/.gitconfig)
+		SUBEMAIL=$(echo $EMAIL | cut -c2-6)
+		if [[ "$SUBEMAIL" = "email" ]]; then
+			break
+		fi
+	done
+	
 	STREND=$(echo $((${#EMAIL}-4)))
 	STRBEG=$((STREND-2))
 	SUB=$(echo $EMAIL | cut -c$STRBEG-$STREND)
-	if [[ "$SITE" = "gitlab" && "git$SUB" != gitlab ]]; then		
-		git config --global user.email GITLAB_EMAIL
-		git config --global user.signingkey GITLAB_SIGNING_KEY
-		echo "Switched to GitLab!"
-	elif [[ "$SITE" = "github" && "git$SUB" != github ]]; then
-		git config --global user.email GITHUB_EMAIL
-		git config --global user.signingkey GITHUB_SIGNING_KEY
-		echo "Switched to GitHub!"
-	fi
+	
+	for i in {1..$[ ${#URL} ]}; do
+		SITE=$(echo $URL | cut -c$[$i]-$[$i+5])
+		if [[ "$SITE" = "gitlab" ]]; then
+			if [[ "git$SUB" != gitlab ]]; then
+				git config --global user.email GITLAB_EMAIL
+				git config --global user.signingkey GITLAB_SIGNING_KEY
+				echo "Switched to GitLab!"
+			fi
+			break
+		elif [[ "$SITE" = "github" ]]; then
+			if [[ "git$SUB" != github ]]; then
+				git config --global user.email GITHUB_EMAIL
+				git config --global user.signingkey GITHUB_SIGNING_KEY
+				echo "Switched to GitHub!"
+			fi
+			break
+		fi
+	done
 fi
